fa671d095112a1a721fa1935db9302a083ab07c0
Alex Hudson <AlexHudson07@gmail.com>
2014-12-11 20:29:22 -0500

    set up app for push

---
+++ SilentRevolution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SilentRevolution
+//
+//  Created by Alex Hudson on 12/11/14.
+//  Copyright (c) 2014 Alex Hudson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SilentRevolution/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SilentRevolution
+//
+//  Created by Alex Hudson on 12/11/14.
+//  Copyright (c) 2014 Alex Hudson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
