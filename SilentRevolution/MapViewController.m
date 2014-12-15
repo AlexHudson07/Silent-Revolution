009af2165578c06e1e4b6358b76d4c7cb4d9c15e
Alex Hudson <AlexHudson07@gmail.com>
2014-12-14 22:09:01 -0500

    done except design

--- SilentRevolution/MapViewController.m
+++ SilentRevolution/MapViewController.m
@@ -3,13 +3,16 @@
 #import <Parse/Parse.h>
 #import "DetailsViewController.h"
 
+
+static int i;
+
 @interface MapViewController () <MKMapViewDelegate>
 @property (strong, nonatomic) IBOutlet MKMapView *mapView;
 @property (strong, nonatomic) IBOutlet UIButton *eventButton;
 @property (strong, nonatomic) IBOutlet UIButton *detailsButton;
 @property (strong, nonatomic) NSArray *locationsArray;
-@property (strong, nonatomic) UIImage *pinImage;
-@property MKPointAnnotation *busAnnotation;
+@property (strong, nonatomic) NSMutableArray *imagesArray;
+
 
 @end
 
@@ -18,15 +21,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-
-
-
-
     CLLocationCoordinate2D coordinate;
     coordinate.latitude = 41.89373984;
     coordinate.longitude = -87.63532979;
 
-    self.busAnnotation = [[MKPointAnnotation alloc] init];
+    self.imagesArray = [NSMutableArray array];
+   // self.busAnnotation = [[MKPointAnnotation alloc] init];
 
     //Design for Buttons
     self.eventButton.layer.cornerRadius = 8;
@@ -58,30 +58,32 @@
 
     PFQuery * query = [PFQuery queryWithClassName: @"Locations"];
 
+    [query orderByAscending:@"Order"];
+
     [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
 
         self.locationsArray = objects.mutableCopy;
 
-        for (PFObject * object in self.locationsArray) {
-            PFGeoPoint * point = object[@"Location"];
+        for (PFObject *object in self.locationsArray) {
+            PFGeoPoint *point = object[@"Location"];
 
             [object[@"pinImage"] getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
 
                 if (point) {
 
-                    self.busAnnotation = [[MKPointAnnotation alloc] init];
+                    MKPointAnnotation *busAnnotation = [[MKPointAnnotation alloc] init];
 
-                    self.pinImage = [UIImage imageWithData:data];
+                    [self.imagesArray addObject:[UIImage imageWithData:data]];
 
                     CLLocationCoordinate2D coordinate;
 
                     coordinate.latitude = point.latitude;
                     coordinate.longitude = point.longitude;
 
-                    _busAnnotation.coordinate = coordinate;
-                    _busAnnotation.title = object[@"Event"];
+                    busAnnotation.coordinate = coordinate;
+                    busAnnotation.title = object[@"Event"];
 
-                    [self.mapView addAnnotation:self.busAnnotation];
+                    [self.mapView addAnnotation:busAnnotation];
                 }
             }];
         }
@@ -91,17 +93,20 @@
 //This Delegate method allowes you to modify the pins and the views above them
 - (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
 {
-    MKPinAnnotationView * pin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:annotation.title];
+    MKPinAnnotationView * pin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"test"];
 
     //shows the business over the pin
     pin.canShowCallout = YES;
 
-    pin.pinColor = MKPinAnnotationColorPurple;
+ //   if (i < self.imagesArray.count){
+    pin.image = [self.imagesArray objectAtIndex:i];
+  //  }
+    i++;
 
-    pin.image = self.pinImage;
+   //  pin.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeContactAdd];
 
-    // pin.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeContactAdd];
-    pin.animatesDrop = YES;
+    
+    //pin.animatesDrop = YES;
 
     return pin;
 }
@@ -125,21 +130,31 @@
     [self.mapView setRegion:region animated:YES];
 }
 
-//map zooms out when map user untaps pin
-- (void)mapView:(MKMapView *)mapView didDeselectAnnotationView:(MKAnnotationView *)view{
-
-    CLLocationCoordinate2D centerCoordinate = view.annotation.coordinate;
-
-    MKCoordinateSpan coordinateSpan;
-    coordinateSpan.latitudeDelta = .25;
-    coordinateSpan.longitudeDelta = .25;
-
-    MKCoordinateRegion region;
-    region.center = centerCoordinate;
-    region.span = coordinateSpan;
-
-    [self.mapView setRegion:region animated:YES];
-}
+////map zooms out when map user untaps pin
+//- (void)mapView:(MKMapView *)mapView didDeselectAnnotationView:(MKAnnotationView *)view{
+//
+//    //CLLocationCoordinate2D centerCoordinate = view.annotation.coordinate;
+//
+//
+//    //Zooms in to Manhattan
+//    CLLocationDegrees longitude = -73.9597;
+//    CLLocationDegrees latitude =  40.7903;
+//
+//    CLLocationCoordinate2D centerCoordinate;
+//
+//    centerCoordinate.longitude = longitude;
+//    centerCoordinate.latitude = latitude;
+//
+//    MKCoordinateSpan coordinateSpan;
+//    coordinateSpan.latitudeDelta = .25;
+//    coordinateSpan.longitudeDelta = .25;
+//
+//    MKCoordinateRegion region;
+//    region.center = centerCoordinate;
+//    region.span = coordinateSpan;
+//
+//    [self.mapView setRegion:region animated:YES];
+//}
 
 - (IBAction)onEventsButtonPressed:(id)sender
 {
